777112e3352a46bfb3e0b2cbc977c8b6a2a93ab2
Aayush Shah <shahaayush349@gmail.com>
2023-12-26 12:46:14 +0530

    chore: add proper test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders marwiz link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/visit our website/i);
   expect(linkElement).toBeInTheDocument();
 });
