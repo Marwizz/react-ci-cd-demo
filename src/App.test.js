76daca98e8d77a7caa010b464fcdd25e06ef512c
Shah-Aayush <shahaayush349@gmail.com>
2023-12-28 16:18:27 +0530

    Jetkins CI: perform integration steps

--- src/App.test.js
+++ src/App.test.js
@@ -1,30 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-console.log("Hello World here and there and everywhere");
-
-
-
-
-
-
 
 test("renders marwiz link", () => {
   render(<App />);
 
-
-
-
-
-
-                const linkElement = screen.getByText(/visit our website/i);
-expect(linkElement).toBeInTheDocument();
-}       
-
-
-
-
-
-
-
-);
+  const linkElement = screen.getByText(/visit our website/i);
+  expect(linkElement).toBeInTheDocument();
+});
